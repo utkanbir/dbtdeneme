f34cb5cc2505950c95d273b10e7b2ce3893d9e56
utolgaevren@yahoo.com <utolgaevren@yahoo.com>
2026-04-22 13:33:22 +0000

    custmer degisikligi 2

--- models/customers.sql
+++ models/customers.sql
@@ -47,7 +47,7 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders1
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
     from customers
 
